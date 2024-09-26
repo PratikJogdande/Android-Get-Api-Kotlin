a3af0ab6ff715d24d52c7ab0ef2efbc7a3d5a43a
PratikJogdande <pratikjogdande7@gmail.com>
2024-09-26 12:35:49 +0530

    Changes in UI

--- app/src/main/java/com/androidgetapikotlin/MainActivity.kt
+++ app/src/main/java/com/androidgetapikotlin/MainActivity.kt
@@ -15,31 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Find the button view by its ID and
-        // assign it to a variable.
         btn_jokes = findViewById(R.id.btn_joke)
-
-        // Find the text view by its ID and
-        // assign it to a variable.
         tv_jokes = findViewById(R.id.tv_joke)
-
-        // Find the progress bar and assign
-        // it to the varriable.
         progressBar = findViewById(R.id.idLoadingPB)
 
-        // Set an OnClickListener on the button view.
         btn_jokes.setOnClickListener {
-            // show the progress bar
             progressBar.visibility = View.VISIBLE
 
-            // Call the getjokes() method of the ApiCall class,
-            // passing a callback function as a parameter.
             ApiCall().getjokes(this) { jokes ->
-                // Set the text of the text view to the
-                // joke value returned by the API response.
                 tv_jokes.text = jokes.value
-                // hide the progress bar
                 progressBar.visibility = View.GONE
             }
         }
